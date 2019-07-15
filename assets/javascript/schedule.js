2b8f79a08bde4a32c66386ce871c4eec0eeb1695
Jesselyn Jackson <jesselyn.jackson@gm.com>
2019-07-15 15:25:09 -0400

    Started making changes at work.

--- assets/javascript/schedule.js
+++ assets/javascript/schedule.js
@@ -20,14 +20,18 @@ $("#submit").click(function(event){
     let destination = $('#inputDestination').val().trim();
     let freq = parseInt($('#inputFrequency').val().trim());
     let firstTrain = $("#inputFirstTrain").val().trim();
+    let nextTrain;
     let arrive;
     let minAway;
     
+    console.log("firstTrain: ", firstTrain);
+    //console.log("Plus 30 min: ", moment(firstTrain, 'hh:mm A').add(30, "minutes").format('hh:mm A'));
 
     let newTrain = {
         name: name,
         destination: destination,
         freq: freq,
+        firstTrain: firstTrain,
         //pulls the current time stamp in UTC
         dateAdded: firebase.database.ServerValue.TIMESTAMP
     }
@@ -44,9 +48,24 @@ $("#submit").click(function(event){
 
 // Firebase watcher .on("child_added"...
 database.ref().on("child_added", function(snap){
-    var value = snap.val();
+    let value = snap.val();
     console.log("value: ", value);
     console.log("name: ", value.name);
-  
+
+    //add logic to calculate next train based on firstTrain + frequency
+    nextTrain = value.firstTrain;
+    console.log("Next train: ", nextTrain);
+    console.log("freguency: ", value.freq);
+
+    do {
+        nextTrain = moment(nextTrain, 'hh:mm A').add(value.freq, 'minutes').format('hh:mm A');
+        // console.log("Next train arrives: ", nextTrain);
+
+    }
+    while(moment(value.dateAdded).format('hh:mm A') > nextTrain);
+    console.log("Next train arrives (after while loop): ", nextTrain);
+
+    //add logic to calculate how long before the train arrives based on next train - current time, maybe use setInterval
+
     $("tbody").append("<tr><td>" + value.name + "</td><td>" + value.destination + "</td><td>" + value.freq + "</td><td>" + "1" + "</td><td>" + "2" + "</td></tr>");
   });
